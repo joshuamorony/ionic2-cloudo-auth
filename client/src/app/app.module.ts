04e0882ad807d356b60a35864ebf9e8981637a85
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2016-12-27 20:44:16 +1030

    initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
+import { Todos } from '../providers/todos';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Todos]
+})
+export class AppModule {}
